a51504d9949ca8de3cc4db21be32c016571c3362
IvoRum <ivailo.rumenov@outlook.com>
2024-03-26 15:37:33 +0200

    Added styles and addons for math and graps. And cofigured the component to load files.

--- fruity-notes-web/src/app/app.component.ts
+++ fruity-notes-web/src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { MarkdownModule, MarkdownService, provideMarkdown } from 'ngx-markdown';
+import {
+  CLIPBOARD_OPTIONS,
+  ClipboardButtonComponent,
+  MarkdownModule,
+  provideMarkdown,
+} from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, MarkdownModule],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, MarkdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [provideMarkdown()],
+  providers: [provideMarkdown({ loader: HttpClient })],
 })
-export class AppComponent {}
+export class AppComponent {
+  [x: string]: any;
+}
+
+provideMarkdown({
+  clipboardOptions: {
+    provide: CLIPBOARD_OPTIONS,
+    useValue: {
+      buttonComponent: ClipboardButtonComponent,
+    },
+  },
+});
